0056a30cb8887a9067260add05a5ecc50496b636
Chris Cummins <chrisc.101@gmail.com>
2016-02-26 14:54:54 +0000

    Bash script overhaul

--- textlint.bash
+++ textlint.bash
@@ -1,7 +1,8 @@
-#! /bin/bash
+#!/usr/bin/env bash
+set -eu
 
 function usage() {
-    echo $0 filename pharovm textlintimage
+    echo "$0 <filename> <pharovm> <textlintimage>"
 }
 
 if [[ -z $1 || -z $2 || -z $3 ]]; then
@@ -10,27 +11,27 @@ if [[ -z $1 || -z $2 || -z $3 ]]; then
 fi
 
 filename="$1"
-# The packaged distribution of pharo crashes for me. Instead, use the
-# system install.
-pharovm=pharo
+pharovm="$2"
 textlintimage="$3"
 
-buffer=$(mktemp /tmp/textlint.XXXX)
+# Temporary buffers:
+tmpdir=/tmp
+input=$(mktemp $tmpdir/textlint.XXXX)
+output=$(mktemp $tmpdir/textlint.XXXX)
 
-cat > "$buffer" <<EOF
-TLConsole checkFileNamed: '$filename' andOutputToFileNamed: 'textlint.log' withinDirectory: '$(pwd)'
+function rm_buffers {
+    rm -f "$input"
+    rm -f "$output"
+}
+trap rm_buffers EXIT
+
+cat > "$input" <<EOF
+TLConsole checkFileNamed: '$filename' andOutputToFileNamed: '$output' withinDirectory: '$(pwd)'
 
 EOF
 
-rm -f textlint.log
-"$pharovm" -headless "$textlintimage" "$buffer"
+# Execute textlint:
+"$pharovm" -headless "$textlintimage" "$input"
 
-rm -f "$buffer"
-
-if [[ ! -e textlint.log ]]; then
-    echo Something bad happened!
-    exit 1
-else
-    cat textlint.log
-    exit 0
-fi
+# Print result:
+cat "$output"
